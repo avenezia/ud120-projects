3c4ae77d1676e1388915e80236521e017e4dc95a
avenezia <alessandro.venezia86@gmail.com>
2016-04-17 15:31:34 +0200

    Completed mail classification using Naive Bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -15,19 +15,24 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+classifier = GaussianNB()
+training_start_time = time()
+classifier.fit(features_train, labels_train)
+print "training time:", round(time() - training_start_time, 3), "s"
 
+prediction_start_time = time()
+predictions = classifier.predict(features_test)
+print "prediction time:", round(time() - prediction_start_time, 3), "s"
 
-
-#########################################################
-### your code goes here ###
-
-
-#########################################################
+accuracy = accuracy_score(labels_test, predictions)
+print accuracy
 
 
