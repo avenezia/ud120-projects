d6cd9987447d3ba8bfca15721058886ae20612e7
avenezia <alessandro.venezia86@gmail.com>
2016-05-07 19:28:36 +0200

    Adding code for explore_enron_data.py

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -15,8 +15,13 @@
     
 """
 
+import math
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
-
+print "Number of people with a known salary", sum([1 for (person, feature) in enron_data.iteritems() if feature["salary"] != "NaN"])
+print "Number of people with a known email", sum([1 for (person, feature) in enron_data.iteritems() if feature["email_address"] != "NaN"])
+people_with_unknown_total_payments = sum([1 for (person, feature) in enron_data.iteritems() if feature["total_payments"] == "NaN" and feature["poi"]])
+print "Number of people with unknown total_payments {}, representing the {} %".format(
+    people_with_unknown_total_payments, people_with_unknown_total_payments * 100.0 / len(enron_data.keys()))
