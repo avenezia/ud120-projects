0d2d8a7cb3fd779e2a51616e93cff4c4ad2d5642
avenezia <alessandro.venezia86@gmail.com>
2016-05-08 16:38:27 +0200

    Completing exercise on linear regression

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -21,7 +21,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
-features_list = ["bonus", "salary"]
+features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -29,20 +29,13 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
-
-### Your regression goes here!
-### Please name it reg, so that the plotting code below picks it up and 
-### plots it correctly. Don't forget to change the test_color above from "b" to
-### "r" to differentiate training points from test points.
-
-
-
-
-
-
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
 
 
 ### draw the scatterplot, with color-coded training and testing points
@@ -57,14 +50,19 @@ plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
 plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 
 
-
-
 ### draw the regression line, once it's coded
 try:
-    plt.plot( feature_test, reg.predict(feature_test) )
+    predictions = reg.predict(feature_test)
+    plt.plot( feature_test, predictions)
+    print "Regression coefficient is {}, intercept is {}".format(reg.coef_, reg.intercept_)
+    print "r^2 score is {}".format(r2_score(target_test, predictions))
 except NameError:
     pass
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
 plt.show()
+
+# Doing the regression on the salary gives a r^2 score of -1.48499241737, that is quite bad.
+# Switching to the long_term_incentive gives a better r^2 (-0.59271289995).
